4b0c323859d8a1a67f871f76cc9b51197be0fa9c
Tailgunnnmer <Tailgunnner@hotmail.com>
2023-11-11 02:50:38 +0300

    glow template

--- GameJam/Scripts/platforms/BaloonPlatform.gd
+++ GameJam/Scripts/platforms/BaloonPlatform.gd
@@ -3,8 +3,10 @@ extends StaticBody2D
 
 @export var bounce_direction  : Vector2 = Vector2.UP
 @export var jump_force : float
+@onready var animator:AnimationPlayer = $AnimationPlayer
 
 
 func _on_jump_area_body_entered(body:Node2D):
 	if body.is_in_group("player"):
+		animator.play('bounce')
 		body.velocity += jump_force * bounce_direction
