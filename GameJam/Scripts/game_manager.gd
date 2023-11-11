1be1f49f27f51da671ab743ae352e0a941b5c182
Tailgunnnmer <Tailgunnner@hotmail.com>
2023-11-11 21:06:28 +0300

    Level 2 and 3 start

--- GameJam/Scripts/game_manager.gd
+++ GameJam/Scripts/game_manager.gd
@@ -5,16 +5,16 @@ var passed_time : float = 0
 @onready var transition_anim : AnimationPlayer = get_node("TransitionCanvas/AnimationPlayer") 
 
 func _ready():
-    transition_anim.play("transition2")
+	transition_anim.play("transition2")
 
 func _process(delta):
-    if Input.is_action_just_pressed("restart"):
-        RestartGame()
+	if Input.is_action_just_pressed("restart"):
+		RestartGame()
 
 func _physics_process(delta):
-    passed_time += delta
+	passed_time += delta
 
 func RestartGame()->void:
-    transition_anim.play("transition1")
-    await get_tree().create_timer(1)
-    get_tree().reload_current_scene()
+	transition_anim.play("transition1")
+	await get_tree().create_timer(1)
+	get_tree().reload_current_scene()
