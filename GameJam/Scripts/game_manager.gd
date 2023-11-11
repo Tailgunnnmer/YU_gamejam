f7baec4eb1c3c6d8f14ed1ab75d1de93c74b1597
Tailgunnnmer <Tailgunnner@hotmail.com>
2023-11-11 18:10:11 +0300

    level stuff

--- GameJam/Scripts/game_manager.gd
+++ GameJam/Scripts/game_manager.gd
@@ -3,10 +3,10 @@ extends Node2D
 
 
 func _process(delta):
-    if Input.is_action_just_pressed("restart"):
-        RestartGame()
+	if Input.is_action_just_pressed("restart"):
+		RestartGame()
 
 
 
 func RestartGame()->void:
-    get_tree().reload_current_scene()
+	get_tree().reload_current_scene()
