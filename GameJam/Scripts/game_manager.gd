e29dd4d2a5bdc9d608055805f88115e3b328d8a6
Arfushs <canberk.soydan@hotmail.com>
2023-11-11 21:18:33 +0300

    Level1

--- GameJam/Scripts/game_manager.gd
+++ GameJam/Scripts/game_manager.gd
@@ -1,20 +1,30 @@
 extends Node2D
 
 @export var expectedTime : float
+@export var ghost_background : ParallaxBackground
+@export var default_background : ParallaxBackground
 var passed_time : float = 0
 @onready var transition_anim : AnimationPlayer = get_node("TransitionCanvas/AnimationPlayer") 
 
 func _ready():
-    transition_anim.play("transition2")
+	transition_anim.play("transition2")
 
 func _process(delta):
-    if Input.is_action_just_pressed("restart"):
-        RestartGame()
+	if Input.is_action_just_pressed("restart"):
+		RestartGame()
 
 func _physics_process(delta):
-    passed_time += delta
+	passed_time += delta
+
+	if LevelData.isInGhost:
+		ghost_background.visible = true
+		default_background.visible = false
+	else:
+		ghost_background.visible = false
+		default_background.visible = true
+
 
 func RestartGame()->void:
-    transition_anim.play("transition1")
-    await get_tree().create_timer(1)
-    get_tree().reload_current_scene()
+	transition_anim.play("transition1")
+	await get_tree().create_timer(1)
+	get_tree().reload_current_scene()
