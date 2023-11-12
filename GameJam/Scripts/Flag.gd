f0ecfbc567f817361ef976fe142cda2a2db67b5c
Tailgunnnmer <Tailgunnner@hotmail.com>
2023-11-12 13:36:58 +0300

    teste12

--- GameJam/Scripts/Flag.gd
+++ GameJam/Scripts/Flag.gd
@@ -6,15 +6,15 @@ var next_level : PackedScene
 
 
 func _ready():
-    next_level = load(next_level_path)
+	next_level = load(next_level_path)
 
 
 func ChangeLevel()->void:
-    get_tree().change_scene_to_packed(next_level)
+	get_tree().change_scene_to_packed(next_level)
 
 func _on_body_entered(body:Node2D):
-    if body.is_in_group("player"):
-        if game_manager.CanPassTheLevel():
-            ChangeLevel()
-        else:
-            game_manager.RestartGame()
+	if body.is_in_group("player"):
+		if game_manager.CanPassTheLevel():
+			ChangeLevel()
+		else:
+			game_manager.RestartGame()
