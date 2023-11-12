c2b699c571edeaebdfbfed81ddbf020139f024a1
Arfushs <canberk.soydan@hotmail.com>
2023-11-12 13:11:12 +0300

    All Levels With Character

--- GameJam/Scripts/player.gd
+++ GameJam/Scripts/player.gd
@@ -8,30 +8,42 @@ extends CharacterBody2D
 
 @export_category("Coyote Time")
 @export var coyote_time : float = 0.05
-var coyote_timer : float 
+var coyote_timer : float
+var isAlive : bool = true 
 
 @onready var bounce_cast : Node2D = get_node("RaycastContainer")
+@onready var anim : AnimatedSprite2D = get_node("AnimatedSprite2D")
 @export var game_manager : Node2D
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity  : float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	isAlive = true
 
 func _physics_process(delta):
-	# Add the gravity.
-	apply_gravity(delta)
-	
-	# Handle Jump.
-	manage_coyote_time()
-	handle_jump()
 
-	check_bounce()
+	if isAlive:
+		# Add the gravity.
+		apply_gravity(delta)
+		
+		# Handle Jump.
+		manage_coyote_time()
+		handle_jump()
 
-	movement()
-	move_and_slide()
+		check_bounce()
 
-	if global_position.y > 1620:
-		RestartLevel()
+		movement()
+		move_and_slide()
+
+		if global_position.y > 1620:
+			RestartLevel()
+
+		if Input.is_action_just_pressed("ui_right"):
+			anim.flip_h = false
+		if Input.is_action_just_pressed("ui_left"):
+			anim.flip_h = true
 	
 
 func apply_gravity(delta : float)->void:
@@ -41,6 +53,7 @@ func apply_gravity(delta : float)->void:
 func handle_jump()->void:
 	if Input.is_action_just_pressed("ui_accept") && coyote_timer>0:
 		velocity.y = jump_force
+		anim.play("Jump")
 
 func manage_coyote_time()->void:
 	if is_on_floor():
@@ -54,8 +67,10 @@ func movement()->void:
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x += accelaration * direction
+		anim.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, deccelaration)
+		anim.play("default")
 		
 	velocity.x = clampf(velocity.x,-MAX_MOVE_SPEED,MAX_MOVE_SPEED)
 
@@ -83,6 +98,9 @@ func SetRayCastCollisionsGhost()->void:
 		ray.set_collision_mask_value(2,true)
 
 func RestartLevel()->void:
+	isAlive = false
+	anim.play("death")
+	await  get_tree().create_timer(0.5).timeout
 	game_manager.RestartGame()
 
 
